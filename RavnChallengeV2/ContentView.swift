63eac54d2832c8c78788f93d84f90197005edbd5
Manuel Sánchez <msanchez@applaudostudios.com>
2020-11-10 17:44:59 -0600

    :tada: Initial Commit

---
+++ RavnChallengeV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RavnChallengeV2
+//
+//  Created by Manuel Sánchez on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RavnChallengeV2/RavnChallengeV2App.swift
@@ -0,0 +1,17 @@
+//
+//  RavnChallengeV2App.swift
+//  RavnChallengeV2
+//
+//  Created by Manuel Sánchez on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RavnChallengeV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
