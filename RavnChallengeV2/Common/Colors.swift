c4bf3c1ab27788e1d7a69f0b56d1fbd68191135a
Manuel Sánchez <msanchez@applaudostudios.com>
2020-11-11 12:09:38 -0600

    :sparkles: Add colors and viewModifiers

---
+++ RavnChallengeV2/Common/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  RavnChallengeV2
+//
+//  Created by Manuel Sánchez on 11/11/20.
+//
+
+import Foundation
---
+++ RavnChallengeV2/Common/Modifiers/TextStyles.swift
@@ -0,0 +1,8 @@
+//
+//  TextStyles.swift
+//  RavnChallengeV2
+//
+//  Created by Manuel Sánchez on 11/11/20.
+//
+
+import Foundation
