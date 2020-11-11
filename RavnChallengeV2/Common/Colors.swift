f75c66e9f79af3c0cdc94806be545922dfcecb4b
Manuel Sánchez <msanchez@applaudostudios.com>
2020-11-11 12:15:00 -0600

    :sparkles: Colors and styles for the text

--- RavnChallengeV2/Common/Colors.swift
+++ RavnChallengeV2/Common/Colors.swift
@@ -5,4 +5,18 @@
 //  Created by Manuel Sánchez on 11/11/20.
 //
 
-import Foundation
+import SwiftUI
+
+enum ColorName {
+    static let ravnBlack = "ravnBlack"
+    static let textDark = "textDark"
+    static let textLight = "textLight"
+    static let textEmphasis = "textEmphasis"
+}
+
+extension Color {
+    static let ravnBlack = Color(ColorName.ravnBlack)
+    static let textDark = Color(ColorName.textDark)
+    static let textLight = Color(ColorName.textLight)
+    static let textEmphasis = Color(ColorName.textEmphasis)
+}
--- RavnChallengeV2/Common/Modifiers/TextStyles.swift
+++ RavnChallengeV2/Common/Modifiers/TextStyles.swift
@@ -5,4 +5,50 @@
 //  Created by Manuel Sánchez on 11/11/20.
 //
 
-import Foundation
+import SwiftUI
+
+struct HeadingStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 17, weight: .bold))
+            .foregroundColor(.textDark)
+    }
+}
+
+struct HeadingHighEmphasisStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 17, weight: .bold))
+            .foregroundColor(.textEmphasis)
+    }
+}
+
+struct HeadingLowEmphasisStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 17, weight: .bold))
+            .foregroundColor(.textLight)
+    }
+}
+
+struct ParagraphStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 14, weight: .regular))
+            .foregroundColor(.textDark)
+    }
+}
+
+struct ParagraphLowEmphasisStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(size: 14, weight: .regular))
+            .foregroundColor(.textLight)
+    }
+}
+
+extension Text {
+    func textStyle<Style: ViewModifier>(_ style: Style) -> some View {
+        ModifiedContent(content: self, modifier: style)
+    }
+}
