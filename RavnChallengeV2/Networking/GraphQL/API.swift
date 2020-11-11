c95475ef5670fef1944309e3fe87191cdb34664c
Manuel Sánchez <msanchez@applaudostudios.com>
2020-11-11 00:19:39 -0600

    :sparkles: Generated API Files

---
+++ RavnChallengeV2/Networking/GraphQL/API.swift
@@ -0,0 +1,252 @@
+// @generated
+//  This file was automatically generated and should not be edited.
+
+import Apollo
+import Foundation
+
+public final class AllPeopleQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query AllPeople($first: Int, $after: String) {
+      allPeople(first: $first, after: $after) {
+        __typename
+        pageInfo {
+          __typename
+          hasNextPage
+          hasPreviousPage
+          startCursor
+          endCursor
+        }
+        people {
+          __typename
+          id
+          name
+        }
+      }
+    }
+    """
+
+  public let operationName: String = "AllPeople"
+
+  public var first: Int?
+  public var after: String?
+
+  public init(first: Int? = nil, after: String? = nil) {
+    self.first = first
+    self.after = after
+  }
+
+  public var variables: GraphQLMap? {
+    return ["first": first, "after": after]
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Root"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("allPeople", arguments: ["first": GraphQLVariable("first"), "after": GraphQLVariable("after")], type: .object(AllPerson.selections)),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(allPeople: AllPerson? = nil) {
+      self.init(unsafeResultMap: ["__typename": "Root", "allPeople": allPeople.flatMap { (value: AllPerson) -> ResultMap in value.resultMap }])
+    }
+
+    public var allPeople: AllPerson? {
+      get {
+        return (resultMap["allPeople"] as? ResultMap).flatMap { AllPerson(unsafeResultMap: $0) }
+      }
+      set {
+        resultMap.updateValue(newValue?.resultMap, forKey: "allPeople")
+      }
+    }
+
+    public struct AllPerson: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["PeopleConnection"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("pageInfo", type: .nonNull(.object(PageInfo.selections))),
+          GraphQLField("people", type: .list(.object(Person.selections))),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(pageInfo: PageInfo, people: [Person?]? = nil) {
+        self.init(unsafeResultMap: ["__typename": "PeopleConnection", "pageInfo": pageInfo.resultMap, "people": people.flatMap { (value: [Person?]) -> [ResultMap?] in value.map { (value: Person?) -> ResultMap? in value.flatMap { (value: Person) -> ResultMap in value.resultMap } } }])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      /// Information to aid in pagination.
+      public var pageInfo: PageInfo {
+        get {
+          return PageInfo(unsafeResultMap: resultMap["pageInfo"]! as! ResultMap)
+        }
+        set {
+          resultMap.updateValue(newValue.resultMap, forKey: "pageInfo")
+        }
+      }
+
+      /// A list of all of the objects returned in the connection. This is a convenience
+      /// field provided for quickly exploring the API; rather than querying for
+      /// "{ edges { node } }" when no edge data is needed, this field can be be used
+      /// instead. Note that when clients like Relay need to fetch the "cursor" field on
+      /// the edge to enable efficient pagination, this shortcut cannot be used, and the
+      /// full "{ edges { node } }" version should be used instead.
+      public var people: [Person?]? {
+        get {
+          return (resultMap["people"] as? [ResultMap?]).flatMap { (value: [ResultMap?]) -> [Person?] in value.map { (value: ResultMap?) -> Person? in value.flatMap { (value: ResultMap) -> Person in Person(unsafeResultMap: value) } } }
+        }
+        set {
+          resultMap.updateValue(newValue.flatMap { (value: [Person?]) -> [ResultMap?] in value.map { (value: Person?) -> ResultMap? in value.flatMap { (value: Person) -> ResultMap in value.resultMap } } }, forKey: "people")
+        }
+      }
+
+      public struct PageInfo: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["PageInfo"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLField("hasNextPage", type: .nonNull(.scalar(Bool.self))),
+            GraphQLField("hasPreviousPage", type: .nonNull(.scalar(Bool.self))),
+            GraphQLField("startCursor", type: .scalar(String.self)),
+            GraphQLField("endCursor", type: .scalar(String.self)),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(hasNextPage: Bool, hasPreviousPage: Bool, startCursor: String? = nil, endCursor: String? = nil) {
+          self.init(unsafeResultMap: ["__typename": "PageInfo", "hasNextPage": hasNextPage, "hasPreviousPage": hasPreviousPage, "startCursor": startCursor, "endCursor": endCursor])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        /// When paginating forwards, are there more items?
+        public var hasNextPage: Bool {
+          get {
+            return resultMap["hasNextPage"]! as! Bool
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "hasNextPage")
+          }
+        }
+
+        /// When paginating backwards, are there more items?
+        public var hasPreviousPage: Bool {
+          get {
+            return resultMap["hasPreviousPage"]! as! Bool
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "hasPreviousPage")
+          }
+        }
+
+        /// When paginating backwards, the cursor to continue.
+        public var startCursor: String? {
+          get {
+            return resultMap["startCursor"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "startCursor")
+          }
+        }
+
+        /// When paginating forwards, the cursor to continue.
+        public var endCursor: String? {
+          get {
+            return resultMap["endCursor"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "endCursor")
+          }
+        }
+      }
+
+      public struct Person: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["Person"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLField("id", type: .nonNull(.scalar(GraphQLID.self))),
+            GraphQLField("name", type: .scalar(String.self)),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(id: GraphQLID, name: String? = nil) {
+          self.init(unsafeResultMap: ["__typename": "Person", "id": id, "name": name])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        /// The ID of an object
+        public var id: GraphQLID {
+          get {
+            return resultMap["id"]! as! GraphQLID
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "id")
+          }
+        }
+
+        /// The name of this person.
+        public var name: String? {
+          get {
+            return resultMap["name"] as? String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+      }
+    }
+  }
+}
